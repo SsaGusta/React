a3360cc93d58c695bc0fb810b5fed8710d74a228
Gusta <gustavoliveiragf15@gmail.com>
2025-07-03 11:25:12 -0300

    feat: add banner in pag

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,10 @@
+import './Banner.css'
+
 function Banner(){
     return(
-        <img src=""
+        <header className="banner">
+          <img src="/imagens/banner.png" alt="o banner principal da pag"/>
+        </header>
     )
 
 
