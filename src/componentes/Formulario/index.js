84c4aabe04bae52d96a2ba089fd53b9d77356bd0
Gusta <gustavoliveiragf15@gmail.com>
2025-07-08 10:35:21 -0300

    feat: little adjustments in code

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,17 @@
+import CampoTexto from "../CampoTexto";
+import "./Formulario.css";
+
 const Formulario = () => {
-
-
-
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Digite o URL da imagem" />
+      </form>
+    </section>
+  )
 }
 
-export default Formulario
+export default Formulario;
