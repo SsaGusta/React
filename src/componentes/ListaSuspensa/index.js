fa4e1eeed92aa32f6f1764853da17dead7b5cc50
Gusta <gustavoliveiragf15@gmail.com>
2025-07-10 12:04:41 -0300

    feat: editing ListaSuspensa

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,19 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+
+
+    return (
+        <div>
+            <label>
+            {props.label}
+            </label>
+            <select>
+                {props.item.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+
+}
+
+export default ListaSuspensa;
