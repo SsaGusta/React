f5a80b941096da0a11bec5f2c883437a90ccfc4f
SsaGusta <gustavoliveiragf15@gmail.com>
2025-09-09 15:17:35 -0300

    add: Css and index for new component

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+
+    return (<div>
+        <div>
+            <img src='' alt='' />
+        </div>
+        <div>
+            <h4>Nome</h4>
+            <h5>Cargo</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
