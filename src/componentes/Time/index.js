2460553ce0d002c8b5654993f9d98ee6d8d8beab
SsaGusta <gustavoliveiragf15@gmail.com>
2025-09-09 14:40:21 -0300

    feat: creating  styles for Times

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,11 @@
 import './time.css';
 
 const Time = (props) => {
+    const css = { borderColor: props.corSecundaria }
     return (
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        
+        <section className='time' style={css}>
+            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
         </section>
     );
 }
